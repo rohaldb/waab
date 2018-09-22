462f0d58b880371c6810cb382ce9a1e135a16666
rohaldbUni <z5019999@ad.unsw.edu.au>
2018-09-22 13:41:45 +1000

    front end changes;

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,20 +1,29 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import Dropzone from 'react-dropzone'
+import Grid from '@material-ui/core/Grid'
+import {withStyles} from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import {DataTable} from './components'
+
+const styles = theme => ({})
 
 class App extends Component {
-  state = { files: [] }
+  state = {
+    files: []
+  }
 
-  onDrop (files) {
-    this.setState({
-      files
-    })
+  onDrop(files) {
+    this.setState({files})
   }
 
   sendFile = () => {
     var data = new FormData()
     data.append('file', this.state.files[0])
     data.append('user', 'hubot')
-    
+
     fetch('http://localhost:5000/', { // Your POST endpoint
       method: 'POST',
       // headers: {
@@ -24,33 +33,48 @@ class App extends Component {
     }).then(response => {
       console.log('responded ', response)
       // response.json() // if the response is a JSON object
-    }).catch(
-      error => console.error(error) // Handle the error response object
+    }).catch(error => console.error(error) // Handle the error response object
     )
   }
 
-  render () {
+  render() {
+    const {classes} = this.props
+
     return (
       <div className='App'>
-        <section>
-          <div className='dropzone'>
-            <Dropzone onDrop={this.onDrop.bind(this)} multiple={false}>
-              <p>Drag a file here or click.</p>
-            </Dropzone>
-          </div>
-          <aside>
-            <h2>Dropped files</h2>
-            <ul>
-              {
-              this.state.files.map(f => <li key={f.name}>{f.name} - {f.size} bytes</li>)
-            }
-            </ul>
-          </aside>
-        </section>
-        <p onClick={() => this.sendFile()}>Go!</p>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="title" color="inherit" className={classes.grow}>
+              WAAB
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Grid container direction="row" justify="center">
+          <Grid item xs={10}>
+            <Grid container direction="column">
+              <Grid item>
+                <Dropzone onDrop={this.onDrop.bind(this)} multiple={false}>
+                  <p>Drag a file here or click.</p>
+                </Dropzone>
+                {this.state.files.map(f => <li key={f.name}>{f.name}
+                  - {f.size}
+                  bytes</li>)
+                }
+              </Grid>
+              <Grid item>
+                <Button onClick={() => this.sendFile()} color="primary" variant="outlined" >
+                  Legooooo
+                </Button>
+              </Grid>
+              <Grid item>
+                <DataTable courses={["COMP2041", "COMP9444", "COMP2121"]} />
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
       </div>
     )
   }
 }
 
-export default App
+export default withStyles(styles)(App)
---
+++ front-end/src/components/dataTable.js
@@ -0,0 +1,80 @@
+import React, {Component} from 'react'
+import { withStyles } from '@material-ui/core/styles'
+import PropTypes from 'prop-types';
+import _ from 'lodash'
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+
+const CustomTableCell = withStyles(theme => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const styles = theme => ({
+  root: {
+      width: '100%',
+      marginTop: theme.spacing.unit * 3,
+      overflowX: 'auto',
+    },
+    table: {
+      minWidth: 700,
+    },
+    row: {
+      '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.background.default,
+      },
+    },
+})
+
+class App extends Component {
+
+  static propTypes = {
+    courses: PropTypes.array.isRequired,
+  }
+
+  render() {
+    const { classes, courses } = this.props
+
+    if (_.size(courses) === 0) {
+      return null
+    }
+    return (
+      <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <CustomTableCell>Name</CustomTableCell>
+            <CustomTableCell numeric>Code</CustomTableCell>
+            <CustomTableCell numeric>Semester(s) Offered</CustomTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {_.map(courses, (x, i) => {
+            return (
+              <TableRow className={classes.row} key={i}>
+                <CustomTableCell component="th" scope="row">
+                  {x}
+                </CustomTableCell>
+                <CustomTableCell numeric>3</CustomTableCell>
+                <CustomTableCell numeric>3</CustomTableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </Paper>
+    )
+  }
+}
+
+export default withStyles(styles)(App)
