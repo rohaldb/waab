dc6928099228c404cf66716b617926d18fa84e3a
aaronsp <aaronspringer95@gmail.com>
2018-09-22 15:50:46 +1000

    merge fix

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -2,23 +2,37 @@ from app import app
 from flask import request, jsonify, json
 from flask_pymongo import PyMongo
 from flask_cors import CORS, cross_origin
+import json
+from flask_pymongo import PyMongo
+from app.programs import *
 from app.services.functions import *
 from app.services.pdf_parser import PdfParser
 
 
 @app.route("/", methods=['GET', 'POST'])
 @cross_origin()
-def hello():
+def home():
     print(len(request.files))
-    print(request.files.get('file'))
     bytestream = request.files.get('file')
     data = PdfParser.open_and_extract(bytestream)
+    program_code = '3707'
+    # with open('./programs.json', 'r') as f:
+    #     program_dict = json.load(f)
+    # print(program_dict)
+    have_to_do = set(programs.get(program_code))
+    already_done = set(data.get('Courses').copy())
+    remaining = have_to_do - already_done
+    # for r in have_to_do:
+    #     if r in already_done:
+    #         remaining.remove(r)
+    data = list(remaining)
     response = app.response_class(
         response=json.dumps(data),
         status=200,
         mimetype='application/json'
     )
     return response
+
 
 @app.route("/courses", methods=["GET", "POST"])
 def get_course():
